4ec8445e3e9a9fc510928415e356fa74d2c29dd7
Andrei Belskii <mrandrewer@yandex.ru>
2023-03-29 14:56:50 +0300

    Add greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-console.log('Brain game working!');
+import readlineSync from 'readline-sync';
+
+console.log('brain-games');
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
