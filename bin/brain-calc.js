d1632f1bb12ca57ac51f5b9e004e7ad28f604ae5
Andrei Belskii <mrandrewer@yandex.ru>
2023-03-30 17:35:58 +0300

    Move calc game logic to /src/games/calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,36 +1,5 @@
 #!/usr/bin/env node
-import { getRandomNumber, runGame } from '../src/game.js';
+import { runGame } from '../src/game.js';
+import { getCalcGameData, getCalcManual } from '../src/games/calc.js';
 
-/**
- * Generates data for calc game turn
- * @returns {Object} An array of strings, where first element is a question
- * and second element is an answer.
- */
-const getCalcGameData = () => {
-  const num1 = getRandomNumber();
-  const num2 = getRandomNumber();
-  const operation = getRandomNumber(3);
-  let question;
-  let answer;
-  switch (operation) {
-    case 1:
-      question = `${num1} + ${num2}`;
-      answer = (num1 + num2).toString();
-      break;
-    case 2:
-      question = `${num1} - ${num2}`;
-      answer = (num1 - num2).toString();
-      break;
-    case 3:
-      question = `${num1} * ${num2}`;
-      answer = (num1 * num2).toString();
-      break;
-    default:
-      question = undefined;
-      answer = undefined;
-      break;
-  }
-  return [question, answer];
-};
-
-runGame('What is the result of the expression?', getCalcGameData);
+runGame(getCalcManual(), getCalcGameData);
---
+++ src/games/calc.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import { getRandomNumber } from '../game.js';
+
+/**
+ * Manual for calc game
+ */
+const getCalcManual = () => 'What is the result of the expression?';
+
+/**
+ * Generates data for calc game turn
+ * @returns {Object} An array of strings, where first element is a question
+ * and second element is an answer.
+ */
+const getCalcGameData = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const operation = getRandomNumber(3);
+  let question;
+  let answer;
+  switch (operation) {
+    case 1:
+      question = `${num1} + ${num2}`;
+      answer = (num1 + num2).toString();
+      break;
+    case 2:
+      question = `${num1} - ${num2}`;
+      answer = (num1 - num2).toString();
+      break;
+    case 3:
+      question = `${num1} * ${num2}`;
+      answer = (num1 * num2).toString();
+      break;
+    default:
+      question = undefined;
+      answer = undefined;
+      break;
+  }
+  return [question, answer];
+};
+
+export { getCalcGameData, getCalcManual }
